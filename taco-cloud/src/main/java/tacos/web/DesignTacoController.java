791309dc365581b4eb2962d736fa98802e2baffa
Nguyen Thanh Nam <thanhnam192@gmail.com>
2019-12-15 23:51:56 +0700

    taco cloud

---
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -0,0 +1,74 @@
+package tacos.web;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.extern.slf4j.Slf4j;
+import tacos.Ingredient;
+import tacos.Ingredient.Type;
+import tacos.Taco;
+import org.springframework.validation.Errors;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        buldIngredents(model);
+        model.addAttribute("design", new Taco());
+        return "design";
+    }
+
+    private void buldIngredents(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+        );
+        Type[] types = Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
+        }
+    }
+
+    @PostMapping
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            buldIngredents(model);
+            return "design";
+        }
+    // Save the taco design...
+    // We'll do this in chapter 3
+            log.info("Processing design: " + design);
+        return "redirect:/orders/current";
+    }
+
+    private List<Ingredient> filterByType(
+            List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+
+
+}
+
---
+++ taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -0,0 +1,38 @@
+package tacos.web;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+//end::baseClass[]
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+//tag::baseClass[]
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+import tacos.Order;
+
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("order", new Order());
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(@Valid @ModelAttribute("order") Order order, Errors errors) {
+        if(errors.hasErrors()){
+            return "orderForm";
+        }
+        log.info("Order submitted: " + order);
+        return "redirect:/";
+    }
+}
